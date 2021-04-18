311ca5fbde3c3e31a8f815bca9eaae6a5cbdbf4b
Sage-Poe <dante.s.poe@gmail.com>
2021-04-18 14:22:54 -0700

    added the other question pages and answer buttons

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var startPage = document.querySelector('.start-page')
-var questionPage = document.querySelector('.question-page')
+var questionPageOne = document.querySelector('.question-page-one')
+var questionPageTwo = document.querySelector('.question-page-two')
+var questionPageThree = document.querySelector('question-page-three')
 var highScorePage = document.querySelector('.high-score-page')
 var startButton = document.querySelector('.start-button')
 var timerEl = document.querySelector('.timer')
 var timeRemaining = 5;
 
+
 function startQuiz (){
     startPage.setAttribute('style', 'display: none;');
-    questionPage.removeAttribute('style');
+    questionPageOne.removeAttribute('style');
+    questionPageOne.setAttribute('style','margin:auto; width:50%; text-align:center;');
     var countdown = setInterval(function(){
         timeRemaining--;
         timerEl.textContent = 'Time Remaining ' + timeRemaining + ' Seconds';
@@ -17,12 +21,16 @@ function startQuiz (){
             clearInterval(countdown);
             //this is where you would call the function that's responsible for taking you to the high scores page//
             startPage.setAttribute('style', 'display: none;');
-            questionPage.setAttribute('style', 'display: none;');
+            questionPageOne.setAttribute('style', 'display: none;');
             highScorePage.removeAttribute('style');
+            highScorePage.setAttribute('style','margin:auto; width:50%; text-align:center;');
             
         }
     },1000)
+
     
+
+
 
 }
 
